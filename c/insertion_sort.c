b9865ef782bd57e5a3af5cf61accccd37055ad3e
Bruce Wang <taolinwant@gmail.com>
2020-03-25 10:33:24 +0800

    change insertion_sort.c

--- c/insertion_sort.c
+++ c/insertion_sort.c
@@ -3,23 +3,32 @@
 
 void insertion_sort(int *arr, int len)
 {
-	int i, j, tmp;
+    int i, j, min;
 
-	for (i = 1; i < len; i++) {
-		for (j = i - 1; j >= 0; j--) {
-			if (arr[j+1] < arr[j]) {
-				tmp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = tmp;
-			}
-		}
-	}
+    for (i = 1; i < len; i++)
+    {
+        min = arr[i - 1];
+        for (j = i; j > 0; j--)
+        {
+            if (arr[j] > arr[j - 1])
+            {
+                min = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = min;
+            }
+        }
+
+    }
 }
+
+
 
 
 int main()
 {
 	int test[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int test2[2] = { 3, 2};
+    int test3[1] = { 3 };
 
 	for (int i = 0; i < 10; i++) {
 		printf("%-3d", test[i]);
@@ -31,6 +40,18 @@ int main()
 
 	for (int i = 0; i < 10; i++) {
 		printf("%-3d", test[i]);
+	}
+	printf("\n");
+
+	insertion_sort(test2, 2);
+	for (int i = 0; i < 2; i++) {
+		printf("%-3d", test2[i]);
+	}
+	printf("\n");
+
+	insertion_sort(test3, 1);
+	for (int i = 0; i < 1; i++) {
+		printf("%-3d", test3[i]);
 	}
 	printf("\n");
 
