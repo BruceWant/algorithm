f6cbb356df15c65a9578ba0f331a42e2cd168fd5
Bruce Wang <taolinwant@gmail.com>
2020-03-31 17:12:58 +0800

    increase max heap

--- c/heapsort/max_heapify.c
+++ c/heapsort/max_heapify.c
@@ -53,21 +53,61 @@ void heap_sort(int *arr, int len)
     }
 }
 
+int heap_max(int *arr)
+{
+    return arr[1];
+}
+
+int extract_max(int *arr, int *len)
+{
+    int max = 0, size = *len - 1;
+    if (*len < 1)
+    {
+        return -1;
+    }
+    max = arr[1];
+    arr[1] = arr[size--];
+    max_heapify(arr, size, 1);
+    *len = size + 1;
+
+    return max;
+}
+
+int increase_key(int *arr, int k, int key)
+{
+    int i = k - 1;
+    if (key < arr[i])
+        return -1;
+    arr[i] = key;
+    while ((i > 1) && (arr[i/2] < arr[i]))
+    {
+        swap(&(arr[i]), &(arr[i/2]));
+        i /= 2;
+    }
+    return i;
+}
 
 int main()
 {
     // int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10};
     int arr[] = {16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
     int len = (int) (sizeof(arr) / sizeof(arr[0]));
+    int max = 0;
 
     for (int i = 1; i < len; i++)
         printf("%-4d", arr[i]);
     printf("\n");
 
-    heap_sort(arr, len);
+    // heap_sort(arr, len);
+    build_heap(arr, len - 1);
+
+    max = extract_max(arr, &len);
+
+    increase_key(arr, 5, 18);
 
     for (int i = 1; i < len; i++)
         printf("%-4d", arr[i]);
     printf("\n");
+    printf("max: %d\n", max);
     return 0;
 }
