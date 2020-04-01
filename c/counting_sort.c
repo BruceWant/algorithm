b10d68be3dde1daef5ce83b1de16f342b4363998
Bruce Wang <taolinwant@gmail.com>
2020-04-01 17:53:17 +0800

    counting sort with bugs in it.

---
+++ c/counting_sort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+void counting_sort(const int *src, int *dst, int len, int max)
+{
+    int cnt[max+1];
+
+    for (int i = 0; i <= max; i++)
+    {
+        cnt[i] = 0;
+    }
+    for (int i = 0; i < len; i++)
+    {
+        cnt[src[i]] = cnt[src[i]] + 1;
+    }
+    for (int i = 1; i <= max; i++)
+    {
+        cnt[i] = cnt[i] + cnt [i-1];
+    }
+
+    for (int i = len-1; i >= 0; i--)
+    {
+        dst[cnt[src[i]]] = src[i];
+        cnt[src[i]] -= 1;
+    }
+}
+
+
+int main()
+{
+    int src[] = { 2, 5, 3, 0, 2, 3, 0, 3 };
+    int len = (int)(sizeof(src) / sizeof(src[0]));
+    int dst[len];
+
+    for (int i = 0; i < len; i++)
+        printf("%-2d", src[i]);
+    printf("\n");
+
+    counting_sort(src, dst, len, 5);
+
+    for (int i = 0; i < len; i++)
+        printf("%-2d", dst[i]);
+    printf("\n");
+
+    return 0;
+}
