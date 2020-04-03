1830efee231170b2d54272c2a30458358941e14f
Bruce Wang <taolinwant@gmail.com>
2020-04-03 17:05:50 +0800

    cnt[i] has the number of the src[number], if want it index, must substract 1 from cnt[i]

--- c/counting_sort.c
+++ c/counting_sort.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+void print(int *arr, int len)
+{
+    for (int i = 0; i < len; i++)
+        printf("%-2d", arr[i]);
+    printf("\n");
+}
 
 void counting_sort(const int *src, int *dst, int len, int max)
 {
@@ -16,6 +22,11 @@ void counting_sort(const int *src, int *dst, int len, int max)
     for (int i = 1; i <= max; i++)
     {
         cnt[i] = cnt[i] + cnt [i-1];
+    }
+    // print(cnt, max+1);
+    for (int i = 0; i <= max; i++)
+    {
+        cnt[i] -= 1;
     }
 
     for (int i = len-1; i >= 0; i--)
@@ -36,6 +47,7 @@ int main()
         printf("%-2d", src[i]);
     printf("\n");
 
+    // 5 is the largest number in src[]
     counting_sort(src, dst, len, 5);
 
     for (int i = 0; i < len; i++)
